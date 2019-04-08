629ca1224eb0ff7b4fdf7d5d215e9e10fec81620
Fernando Ferreira <fferreira12@gmail.com>
2019-04-08 13:09:18 -0300

    Adds Timeline

---
+++ TwitterClone/Timeline.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterClone
+{
+    public class Timeline
+    {
+
+        Stack<Tweet> Tweets;
+
+        public Timeline()
+        {
+            Tweets = new Stack<Tweet>();
+        }
+
+        public void AddTweet(Tweet tweet)
+        {
+            Tweets.Push(tweet);
+        }
+
+        public List<Tweet> GetLatestNTweets(int quantity)
+        {
+            if (Tweets.Count <= quantity)
+            {
+                return Tweets.ToList();
+            }
+            else
+            {
+                return Tweets.Take(quantity).ToList();
+            }
+        }
+
+    }
+}
---
+++ UnitTests/TestTimeline.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using TwitterClone;
+
+namespace Tests
+{
+    public class TestTimeline
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestCreateTimeline()
+        {
+            Timeline timeline = new Timeline();
+        }
+
+        [Test]
+        public void TestAddTweet()
+        {
+            Timeline timeline = new Timeline();
+
+            User user = new User("a", "b", "c");
+            Tweet tweet = new Tweet(user, "This is a tweet");
+
+            timeline.AddTweet(tweet);
+
+            //Assert.AreEqual(1, timeline.GetLatestNTweets(1).Count);
+        }
+
+        [Test]
+        public void TestAdd2Tweets()
+        {
+            Timeline timeline = new Timeline();
+
+            User user1 = new User("ash", "boyd", "c@gmail.com");
+            User user2 = new User("dale", "eve", "f@gmail.com");
+            Tweet tweet1 = new Tweet(user1, "This is a tweet 1");
+            Tweet tweet2 = new Tweet(user2, "This is a tweet 2");
+
+            timeline.AddTweet(tweet1);
+            timeline.AddTweet(tweet2);
+
+            List<Tweet> tweets = timeline.GetLatestNTweets(2);
+
+            //Since it is a stack, the last added tweet should be the first in the list.
+            Assert.AreEqual("dale", tweets[0].User.FirstName);
+            Assert.AreEqual("ash", tweets[1].User.FirstName);
+        }
+
+        [Test]
+        public void TestGetTweets()
+        {
+            Timeline timeline = new Timeline();
+
+            User user = new User("a", "b", "c");
+            Tweet tweet = new Tweet(user, "This is a tweet");
+
+            timeline.AddTweet(tweet);
+
+            Assert.AreEqual(1, timeline.GetLatestNTweets(1).Count);
+        }
+
+    }
+
+}
